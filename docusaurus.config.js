5f5d17dbdd1c5616784fd02b09e2a9542a825ef5
Yogesh Sharma <yogesh.sharma@rediffmail.com>
2024-08-29 15:39:23 +0530

    Checking

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -132,7 +132,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com',
               },
             ],
           },
